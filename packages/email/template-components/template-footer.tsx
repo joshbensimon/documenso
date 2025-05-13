98e5e8d4ce7d60be7871a98a2e2dde012b6bd724
Josh Bensimon <jjbensimon@me.com>
2025-05-13 17:58:39 +0200

    revert branding

--- packages/email/template-components/template-footer.tsx
+++ packages/email/template-components/template-footer.tsx
@@ -12,7 +12,18 @@ export const TemplateFooter = ({ isDocument = true }: TemplateFooterProps) => {
 
   return (
     <Section>
-      {isDocument && !branding.brandingHidePoweredBy && branding.brandingCompanyDetails ? (
+      {isDocument && !branding.brandingHidePoweredBy && (
+        <Text className="my-4 text-base text-slate-400">
+          <Trans>
+            This document was sent using{' '}
+            <Link className="text-[#7AC455]" href="https://documen.so/mail-footer">
+              Documenso.
+            </Link>
+          </Trans>
+        </Text>
+      )}
+
+      {branding.brandingCompanyDetails ? (
         <Text className="my-8 text-sm text-slate-400">
           {branding.brandingCompanyDetails.split('\n').map((line, idx) => {
             return (
@@ -23,7 +34,13 @@ export const TemplateFooter = ({ isDocument = true }: TemplateFooterProps) => {
             );
           })}
         </Text>
-      ) : null}
+      ) : (
+        <Text className="my-8 text-sm text-slate-400">
+          Documenso, Inc.
+          <br />
+          2261 Market Street, #5211, San Francisco, CA 94114, USA
+        </Text>
+      )}
     </Section>
   );
 };
