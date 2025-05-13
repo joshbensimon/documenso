72c0cc2541fb35cf4931599b7945386dbbbd8802
Josh Bensimon <jjbensimon@me.com>
2025-05-13 19:39:43 +0200

    added branding to sing in page

--- apps/remix/app/routes/_unauthenticated+/signin.tsx
+++ apps/remix/app/routes/_unauthenticated+/signin.tsx
@@ -10,6 +10,7 @@ import {
 import { env } from '@documenso/lib/utils/env';
 
 import { SignInForm } from '~/components/forms/signin';
+import { BrandingLogo } from '~/components/general/branding-logo';
 import { appMetaTags } from '~/utils/meta';
 
 import type { Route } from './+types/signin';
@@ -43,13 +44,14 @@ export default function SignIn({ loaderData }: Route.ComponentProps) {
   return (
     <div className="w-screen max-w-lg px-4">
       <div className="border-border dark:bg-background z-10 rounded-xl border bg-neutral-100 p-6">
+        <div className="flex justify-center mb-4">
+          <BrandingLogo className="h-8 w-auto" />
+        </div>
+        
         <h1 className="text-2xl font-semibold">
           <Trans>Sign in to your account</Trans>
         </h1>
 
-        <p className="text-muted-foreground mt-2 text-sm">
-          <Trans>Welcome back, we are lucky to have you.</Trans>
-        </p>
         <hr className="-mx-6 my-4" />
 
         <SignInForm
